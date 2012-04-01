77c0000a8c1ebcbeec70c86cf24f4bf8fbf7483a
Ryan Fang <zffl@yahoo.cn>
2012-04-01 06:56:41 -0400

    add debug

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import web
 
-db = web.database(dbn='sqlite', db='todo')
+db = web.database(dbn='sqlite', db='todo.db')
 
 '''
 CREATE TABLE todo (
@@ -8,24 +8,31 @@ CREATE TABLE todo (
     title TEXT,
     user TEXT,
     developerstatus INTEGER,
-    qastatus INTEGER
+    qastatus INTEGER,
+    backupnum INTEGER
 );
 '''
-def get_todos():
-    return db.select('todo', order='user') #, order='qastatus desc, developerstatus desc'
+def get_todos(debug=0):
+    where = "backupnum=0"
+    if debug == 1:
+        where = "backupnum!=0"
+    return db.select('todo', order='user', where=where) #, order='qastatus desc, developerstatus desc'
 
 def new_todo(text,user):
-    db.insert('todo', title=text, user=user, developerstatus=1,qastatus=1)
+    db.insert('todo', title=text, user=user, developerstatus=1,qastatus=1,backupnum=0)
 
 def developer_done_todo(id,status):
     db.update('todo', where="id=$id", vars=locals(), developerstatus=status)
-	
+
 def qa_done_todo(id,status):
     db.update('todo', where="id=$id", vars=locals(), qastatus=status)
-	
+    
+def sprint_backup(backupnum):
+    db.update('todo',where='developerstatus=0 and qastatus=0',backupnum=backupnum)
+
 def delete(id):
     db.delete('todo', where="id=$id", vars=locals())
-	
+
 
 '''
 CREATE TABLE user (
--- todo.py
+++ todo.py
@@ -5,7 +5,8 @@ urls = (
     '/', 'Index',
     '/developer_done/(\d+)', 'developer_done',
     '/qa_done/(\d+)', 'qa_done',
-    '/delete/(\d+)', 'delete'
+    '/delete/(\d+)', 'delete',
+    '/backup/(\d+)','backup'
 )
 
 render = web.template.render('templates', base='base')
@@ -14,20 +15,20 @@ def getUserbyIP():
     return model.getUserbyIP(web.ctx.ip)
 
 class Index:
-        
     def GET(self):
+        debug = int(web.webapi.input(debug=0).debug)
         user = getUserbyIP()
         isqa = model.is_qa(user)
-        todos = model.get_todos()
-        return render.index(todos, user, isqa)
+        todos = model.get_todos(debug)
+        return render.index(todos, user, isqa, debug)
 
     def POST(self):
         user = getUserbyIP()
-        model.new_todo(web.webapi.input().title,user)
+        title = web.webapi.input().title;
+        model.new_todo(title,user)
         raise web.seeother('/')
 
 class developer_done:
-
     def POST(self, id):
         id = int(id)
         status = 1
@@ -37,7 +38,6 @@ class developer_done:
         raise web.seeother('/')
 
 class qa_done:
-
     def POST(self, id):
         id = int(id)
         status = 1
@@ -47,10 +47,15 @@ class qa_done:
         raise web.seeother('/')
 
 class delete:
-
     def POST(self, id):
         id = int(id)
         model.delete(id)
+        raise web.seeother('/')
+    
+class backup:
+    def GET(self, id):
+        id = int(id)
+        model.sprint_backup(id)
         raise web.seeother('/')
 
 app = web.application(urls, globals())
