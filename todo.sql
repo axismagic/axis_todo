21be961907aa8f37bb1e6b93539be28180d1ad9a
Ryan Fang <zffl@yahoo.cn>
2012-04-01 03:31:28 -0400

    fix ignore file

--- todo.sql
+++
@@ -1,9 +0,0 @@
---'Austin', 'Bill', 'Emma', 'Fiona', 'Ryan', 'Seven', 'Wade'
-insert into user values (1,'Austin','1','','cns-austin');
-insert into user values (2,'Bill','1','','cnshhq-0280');
-insert into user values (3,'Emma','2','','CNSHHQ-0239');
-insert into user values (4,'Fiona','2','','CNS-882');
-insert into user values (5,'Ryan','1','','CNS-881');
-insert into user values (6,'Seven','1','','cns-783');
-insert into user values (7,'Wade','1','','cns-545');
-insert into user values (8,'Patrick','3','','');
